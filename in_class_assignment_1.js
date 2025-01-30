286f2d97419bf20ad8384eaa3dd66d102faa52d9
Your Name <your.email@example.com>
2025-01-30 16:17:08 -0500

    Task 1 and 2

--- in_class_assignment_1.js
+++ in_class_assignment_1.js
@@ -1,2 +1,22 @@
 // Task 1
 
+let x = 10
+if (x > 5) {
+    console.log("Help")
+}
+
+let expenses = 10000;
+if (expenses > 7000) {
+    console.log("High Expenses")
+} else {
+    console.log("Manageable Expenses")
+}
+
+// Task 2 Practice Problem
+let score = 80
+if (score >= 60) {
+    console.log("Passed")
+} else {
+    console.log("Failed")
+}
+
